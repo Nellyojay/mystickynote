a7aba03c0bcef3c35f69bde701e1af99b6befd91
Nellyojay <nellyjohnson696@gmail.com>
2026-09-20 14:37:57 +0300

    Added the installable app

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa/dist/index.cjs'
 
 export default defineConfig({
   base: '/mystickynote/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+
+    manifest: {
+      name: 'MyStickyNote',
+      short_name: 'MyStickyNote',
+      description: 'A simple sticky note application',
+      theme_color: '#f7d77a',
+      background_color: '#f7d77a',
+      display: 'standalone',
+      start_url: '/mystickynote/',
+      icons: [
+        {
+          src: 'src/assets/images/mystickynote_app_icon.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'src/assets/images/mystickynote_app_icon.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
 })
